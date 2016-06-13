aa98e486fbd62e226f4ed6243b57cabf29bffcc9
Monsonis <monsonis@gmail.com>
2016-06-13 10:59:10 +0200

    Añadir al bootstrap.js un ejemplo de parseo

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,12 @@
 import argparse from './lib/argparse'
+import fs from 'fs'
+import handlebars from 'handlebars'
 
 const args = argparse.getArgs(process.argv)
-
 console.log(args)
+
+var source = fs.readFileSync('./templates/java/dao.handlebars', 'utf8');
+var template = handlebars.compile(source)
+var data = { "modelName": "Hospital", "instanceName": "hospital", "applicationName": "pho"};
+var result = template(data);
+console.log(result);
